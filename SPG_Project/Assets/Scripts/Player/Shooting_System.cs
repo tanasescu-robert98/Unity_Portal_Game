976e37cc695d248787ff1d3f627e2e745c9a331c
tanasescu-robert98 <robytupa@yahoo.com>
2021-12-31 17:56:29 +0200

    added raycast for weapon

--- SPG_Project/Assets/Scripts/Player/Shooting_System.cs
+++ SPG_Project/Assets/Scripts/Player/Shooting_System.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Shooting_System : MonoBehaviour
 {
     public ParticleSystem MuzzleFlash;
+    public Camera fpsCam;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,17 @@ public class Shooting_System : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             MuzzleFlash.Play();
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit Hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out Hit))
+        {
+            Debug.Log(Hit.transform.name);
+            //Destroy(Hit.transform.gameObject);
         }
     }
 }
